0b0a2eb2698f4705cd905cc6838c8ff48cb90b41
gravilaxe <joshuawright3132@outlook.com>
2023-02-09 15:23:56 +0000

    Initial Commit

---
+++ CodingTeacher/CodingTeacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingTeacherApp.swift
+//  CodingTeacher
+//
+//  Created by Joshua Wright on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodingTeacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
